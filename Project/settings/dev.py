0141018c30c5796731230f552dc9dddfdde4991f
Bissane <Bissane@Bissane.local>
2019-02-20 12:10:43 +0100

    First commit

---
+++ Project/settings/dev.py
@@ -0,0 +1,15 @@
+from Project.settings.base import *
+
+# Override base.py settings here
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+try:
+    from Project.settings.local import *
+except:
+    pass
